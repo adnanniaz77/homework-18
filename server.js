a116e013d8ee98e9faeb4821160670dedc597301
Adnan Niaz <adnanniaz77@yahoo.com>
2020-02-11 21:12:28 -0800

    added express routers

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use("/api", require("./routes/api-routes"));
+
+app.listen(PORT, () => {
+    console.log("listening at http://localhost:" + PORT);
+});
