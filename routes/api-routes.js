7a05f76c64831bc1be907ed048f4be3e6729d7a2
Adnan Niaz <adnanniaz77@yahoo.com>
2020-02-12 02:30:20 -0800

    successfully added get and post methods for the schema

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,16 +2,21 @@ const express = require("express");
 const router = express.Router();
 const Article = require("../models/Article");
 
-router.get("/", (req, res) => {
-    res.send("hi");
-});
-
+// Get all Articles
 router.get("/all", (req, res) => {
     Article.find()
         .then(article => res.send(article))
         .catch(e => console.log(e));
 });
 
-// router.post("/", (req, res) => {});
+router.post("/create", (req, res) => {
+    Article.create({
+        title: req.body.title,
+        text: req.body.text,
+        link: req.body.link
+    })
+        .then(newArticle => res.send(newArticle))
+        .catch(err => console.log(err));
+});
 
 module.exports = router;
