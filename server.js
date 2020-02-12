3e3701983fa1cef1cff71bab23ccb6553897c670
Adnan Niaz <adnanniaz77@yahoo.com>
2020-02-11 21:31:46 -0800

    successfully connected to mongoDB

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const mongoose = require("mongoose");
+const connection = mongoose.connection;
+
+mongoose.connect(process.env.MONGO_URI || "mongodb://localhost/class-work", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
+
+connection.on("error", err => console.log(err));
+connection.once("open", () => console.log("Connected to mongoDB"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
